226b1a7a5322ffc05cd027790e1757684f218b02
Kanishk19092003 <kanishkyadav141@gmail.com>
2026-01-29 20:05:21 +0530

    Implement admin registration with bcrypt password hashing

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const Post = require("../models/Post");
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const adminLayout = '../views/layouts/admin';
 
@@ -19,7 +21,6 @@ router.get('/admin',(req,res)=>{
 });
 
 //POST Admin - check login//
-
 router.post('/admin',async (req,res)=>{
     try {
         const {username,password} = req.body;
@@ -32,5 +33,28 @@ router.post('/admin',async (req,res)=>{
         console.log(error);
     }
 })
+
+
+//POST Admin - register//
+router.post('/register', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    try {
+      const user = await User.create({ username, password:hashedPassword });
+      res.status(201).json({ message: 'User Created', user });
+    } catch (error) {
+      if(error.code === 11000) {
+        res.status(409).json({ message: 'User already in use'});
+      }
+      res.status(500).json({ message: 'Internal server error'})
+    }
+
+  } catch (error) {
+    console.log(error);
+  }
+});
+
 
 module.exports= router;
